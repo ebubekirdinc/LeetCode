da177b2cef04493d7781fdd723d67924274ce578
ebubekir dinç <Ebubekirdinc@gmail.com>
2020-01-09 17:29:21 +0300

    last update

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,23 +6,23 @@ namespace LeetCode
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             //int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-            int[] nums = { -1, 0, 0, 0, 0, 3, 3 };
+            //int[] nums = { 1, 2 };
             //int[] nums = { 1, 1, 2 };
-            //int[] nums = { 0, 0, 0, 0, 3 };  
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
             int[] prices = { 7, 1, 5, 3, 6, 4 };
 
             ArrayProblems array = new ArrayProblems();
+            array.Rotate(nums, 3);
 
             //Console.WriteLine(sl.RemoveDuplicates(nums).Count());
             //Console.WriteLine("--------------");
-            //foreach (var item in nums)
-            //{
-            //    Console.WriteLine(item);
-            //}
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
 
-            //Console.WriteLine(array.MaxProfit(prices));
 
 
         }
@@ -85,13 +85,43 @@ namespace LeetCode
         }
 
         /// <summary>
-        /// Rotate Array
+        /// Given an array, rotate the array to the right by k steps, where k is non-negative.
         /// </summary>
         /// <param name="nums"></param>
         /// <param name="k"></param>
-        public void Rotate(int[] nums, int k)
+        public int[] Rotate(int[] nums, int k)
         {
+            if (nums.Length == 1)
+                return nums;
 
+            int swap = 0;
+            int swap1 = 0;
+            int j = 0;
+            int s = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (s + k < nums.Length)
+                {
+                    swap1 = nums[s + k];
+                    nums[s + k] = swap == 0 ? nums[s] : swap;
+                    swap = swap1;
+                    s = s + k;
+                }
+                else
+                {
+                    swap1 = j == 0 ? nums[s + k - nums.Length] : nums[s + k];
+                    nums[s + k - nums.Length] = swap == 0 ? nums[s] : swap;
+                    swap = swap1;
+                    s = j == 0 ? s + k - nums.Length : s + k;
+                    j++;
+                }
+
+
+
+            }
+
+
+            return nums;
         }
     }
 }
